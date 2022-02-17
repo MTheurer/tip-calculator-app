10eca193c92240b9f373bda72751870d83406dc7
Makenzie Theurer <mtheurer@octolabs.io>
2022-02-17 14:17:13 -0500

    component changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ButtonComponent } from './components/button/button.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ButtonComponent,
+  ],
+  exports: [
     ButtonComponent
   ],
   imports: [
