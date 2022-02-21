369fc282eb2c1713be2443b86167490c055ac7e6
Makenzie Theurer <mtheurer@octolabs.io>
2022-02-21 12:24:11 -0500

    input and button functionality additions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tip-calculator-app';
+
+  togglePercent(){
+    console.log("percentage clicked")
+  }
+  getCustomPercent(percent:string){
+    console.log(percent);
+  }
+  getBillTotal(bill:string){
+  console.log(bill);
+  }
+
+  getTotalPeople(people:string){
+    console.log(people);
+  }
+
+  /*calculateTotals(){
+    let billPerPerson = bill/people
+    let tipAmount = billPerPerson*percent
+    let totalPerPerson = billPerPerson + tipAmount
+  }*/
 }
